b8e8cde90d336179348b954a3e86b7b7815f5c8c
yonglihu <ylhu@thoughtworks.com>
2021-08-20 21:36:15 +0800

    Remove bottom navigation bar

--- app/src/main/java/com/laohu/study/platform/page/home/ui/HomeScreen.kt
+++ app/src/main/java/com/laohu/study/platform/page/home/ui/HomeScreen.kt
@@ -1,9 +1,6 @@
 package com.laohu.study.platform.page.home.ui
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -14,23 +11,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.laohu.study.platform.R
-import com.laohu.study.platform.model.CoursePhase
-import com.laohu.study.platform.page.home.viewmodel.HomeViewModel
 import com.laohu.study.platform.ui.theme.StudyPlatformTheme
 import kotlinx.coroutines.launch
 
@@ -51,15 +36,8 @@ fun HomeScreen() {
                 }
             })
         },
-        bottomBar = {
-            HomeBottomNavigation(onBottomNavItemChanged = {
-
-            })
-        },
         content = {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text(text = "MainScreen", color = Color.Black)
-            }
+            PhaseScreen(phaseCode = "111")
         }
     )
 }
@@ -79,36 +57,6 @@ fun HomeTopAppBar(openDrawer: () -> Unit) {
             }
         },
     )
-}
-
-@Composable
-fun HomeBottomNavigation(
-    onBottomNavItemChanged: (CoursePhase) -> Unit,
-    homeViewModel: HomeViewModel = viewModel()
-) {
-    val currentOnBottomNavItemChanged by rememberUpdatedState(onBottomNavItemChanged)
-    BottomNavigation(
-        backgroundColor = MaterialTheme.colors.primary,
-        contentColor = Color.White
-    ) {
-        val phases by homeViewModel.coursePhase.observeAsState(emptyList())
-        var currentIndex by rememberSaveable { mutableStateOf(0) }
-        phases.forEachIndexed { index, phase ->
-            BottomNavigationItem(
-                icon = { Icon(painterResource(R.drawable.ic_school), "") },
-                label = { Text(text = phase.phaseName) },
-                selected = currentIndex == index,
-                selectedContentColor = Color.White,
-                unselectedContentColor = Color.White.copy(0.4f),
-                onClick = {
-                    if (currentIndex != index) {
-                        currentIndex = index
-                        currentOnBottomNavItemChanged(phase)
-                    }
-                }
-            )
-        }
-    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
---
+++ app/src/main/java/com/laohu/study/platform/page/home/ui/PhaseScreen.kt
@@ -0,0 +1,9 @@
+package com.laohu.study.platform.page.home.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PhaseScreen(phaseCode: String) {
+    Text(text = phaseCode)
+}
